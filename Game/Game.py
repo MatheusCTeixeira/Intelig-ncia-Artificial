a6bbe72b3278592596ee26b600a9a3bfbdbe1a3e
Matheus Cândido Teixeira <matheuscandido2009@gmail.com>
2018-10-15 19:39:30 -0400

    update

--- Game/Game.py
+++ Game/Game.py
@@ -5,70 +5,67 @@ from PyQt5 import QtCore
 
 from PyQt5.QtChart import QBarSeries, QBarSet, QChart, QChartView, QBarCategoryAxis
 
-from deprecated import deprecated
-
 #--------------------------------------------------------------------------#
 
 
 class Piece(QLabel):
     """ Representa uma peça no tabuleiro """
     matrix_piece = []
-    map = [[]]
 
-    def __init__(self, text, position, map):
+    def __init__(self, text, position, board, pieces):
         super().__init__()
         super().setText(text)
 
         self.position = position
-        self.map = map
-        print(self.map)
+        self.board = board
+        self.pieces = pieces
 
-        i, j = self.position
-        while len(self.matrix_piece) <= i:
-            self.matrix_piece.append([])
+        print(self.board)
 
-        while len(self.matrix_piece[i]) <= j:
-            self.matrix_piece[i].append(self)
+        #i, j = self.position
+        #while len(self.matrix_piece) <= i:
+        #    self.matrix_piece.append([])
+#
+        #while len(self.matrix_piece[i]) <= j:
+        #    self.matrix_piece[i].append(self)
 
-    @deprecated(version='1', reason='inelegant. replaced by update_pieces()')
     def swap_text(self, lbl1, lbl2):
         text = lbl1.text()
         lbl1.setText(lbl2.text())
         lbl2.setText(text)
   
-    def update_pieces(self): 
-        print(Piece.map)
-        for i in range(len(Piece.matrix_piece)):
-            for j in range(len(Piece.matrix_piece[0])):
-                Piece.matrix_piece[i][j].setText(str(self.map[i][j]))
+    def update_pieces(self):         
+        for i in range(len(self.pieces)):
+            for j in range(len(self.pieces[0])):
+                self.pieces[i][j].setText(str(self.board[i][j]))
 
     def swap_map(self, pos1, pos2):
         i1, j1 = pos1
         i2, j2 = pos2
 
-        val = self.map[i1][j1]
-        self.map[i1][j1] = self.map[i2][j2]
-        self.map[i2][j2] = val
+        val = self.board[i1][j1]
+        self.board[i1][j1] = self.board[i2][j2]
+        self.board[i2][j2] = val
 
     def mousePressEvent(self, mouse_event):
         """ Processa o evento de click em um peça do tabuleiro """
         i, j = self.position
 
-        if i > 0 and self.map[i - 1][j] == 0:
+        if i > 0 and self.board[i - 1][j] == 0:
             self.swap_map(self.position, (i - 1, j))
 
-        elif i + 1 < len(self.map) and self.map[i + 1][j] == 0:
+        elif i + 1 < len(self.board) and self.board[i + 1][j] == 0:
             self.swap_map(self.position, (i + 1, j))
 
-        elif j > 0 and self.map[i][j - 1] == 0:
+        elif j > 0 and self.board[i][j - 1] == 0:
             self.swap_map(self.position, (i, j - 1))
 
-        elif j + 1 < len(self.map[0]) and self.map[i][j + 1] == 0:
+        elif j + 1 < len(self.board[0]) and self.board[i][j + 1] == 0:
             self.swap_map(self.position, (i, j + 1))
 
         self.update_pieces()
 
-        for line in self.map:
+        for line in self.board:
             for x in line:
                 print("%3d" % x, end='')
             print()
@@ -77,41 +74,43 @@ class Piece(QLabel):
 
 #--------------------------------------------------------------------------#
 
-
-class SolutionManager:
-    """ Esta classe é resposável por controlar a exibição da solução passo a passo """
-
-    def __init__(self, solution, map):
-        self.solution = solution
-        self.step = 0
-
-    def step_foward(self):
-        if (len(self.solution) + 1 < self.step):
-            self.step += 1
-
-    def step_back(self):
-        if (self.step > 0):
-            self.step -= 1
-
-    def get_step(self):
-        if (self.solution != None and len(self.solution) > self.step):
-            return self.solution[self.step]
-
+#
+#class SolutionManager:
+#    """ Esta classe é resposável por controlar a exibição da solução passo a passo """
+#
+#    def __init__(self, solution):
+#        self.solution = solution
+#        self.step = 0        
+#
+#    def step_foward(self):
+#        
+#
+#        if (len(self.solution) + 1 < self.step):
+#            self.step += 1
+#
+#    def step_back(self):
+#        if (self.step > 0):
+#            self.step -= 1
+#
+#    def get_step(self):
+#        if (self.solution != None and len(self.solution) > self.step):
+#            return self.solution[self.step]
+#
 
 #--------------------------------------------------------------------------#
 
 
 class ControlButton(QPushButton):
-    """ Representa os buttons para iterar a solução """
-    pieces = None
-    board = [[]]
+    """ Representa os buttons para iterar a solução """    
 
-    def __init__(self, text, action):
+    def __init__(self, text, action, board, pieces):
         super().__init__()
         super().setText(text)
 
         self.action = action
-
+        self.board = board
+        self.pieces = pieces    
+        
     def mouseReleaseEvent(self, event):
         """ Ação de clicar no button """
         if self.action == "solution":
@@ -124,13 +123,34 @@ class ControlButton(QPushButton):
         super().mouseReleaseEvent(event)
 
     def find_solution(self):
-        print("solution")
+        pass
     
     def foward(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                self.board[i][j] = 1
+
+        self.board[0][0] = 0
+
+        self.update_pieces()
+
         print("foward")
 
     def back(self):
         print("back")
+
+    def update_pieces(self):         
+        for i in range(len(self.pieces)):
+            for j in range(len(self.pieces[0])):
+                self.pieces[i][j].setText(str(self.board[i][j]))
+
+    def swap_map(self, pos1, pos2):
+        i1, j1 = pos1
+        i2, j2 = pos2
+
+        val = self.board[i1][j1]
+        self.board[i1][j1] = self.board[i2][j2]
+        self.board[i2][j2] = val
 #--------------------------------------------------------------------------#
 
 
@@ -139,15 +159,13 @@ class Game:
 
     def __init__(self, N):
         app = QApplication([])
-
-        self.current_step = 0
+        
         self.N = N
 
         vLayout = QVBoxLayout()
-        vLayout.addWidget(
-            QLabel("<h1><center>Quebra-Cabeça N Peças</center></h1>"))
+        vLayout.addWidget(QLabel("<h1><center>Quebra-Cabeça N Peças</center></h1>"))
         vLayout.addWidget(self.create_board(self.N))
-        vLayout.addWidget(self.step_solution())
+        vLayout.addWidget(self.create_controls())
         vLayout.addWidget(self.solution_methods())
 
         hLayout = QHBoxLayout()
@@ -167,7 +185,8 @@ class Game:
         """ Cria o tabuleiro """
 
         self.board = [[a+b*N for a in range(N)] for b in range(N)]
-        piece_size = 70
+        self.pieces = [[] for b in range(N)]
+        piece_size = 50
 
         pane = QWidget()
         pane.setStyleSheet("border: 1px solid black;")
@@ -175,7 +194,7 @@ class Game:
         grid = QGridLayout()
         for i in range(len(self.board)):
             for j in range(len(self.board[0])):
-                piece = Piece(str(self.board[i][j]), (i, j), self.board)
+                piece = Piece(str(self.board[i][j]), (i, j), self.board, self.pieces)
                 piece.setAlignment(QtCore.Qt.AlignCenter)
                 piece.setFont(QFont("source code pro", 20))
                 piece.setFixedSize(piece_size, piece_size)
@@ -183,6 +202,8 @@ class Game:
                 piece.setStyleSheet("border: 1px solid red;\
                                      background: yellow;   \
                                      border-radius: 4px;")
+                                     
+                self.pieces[i].append(piece)                                     
                 grid.addWidget(piece, i, j)
 
         pane.setLayout(grid)
@@ -191,15 +212,15 @@ class Game:
     def solution_methods(self):
         """ Adiciona as opções de soluções utilizando IA """
 
-        cbbMethods = QComboBox()
-        cbbMethods.addItems(["BFS", "DFS Iter.", "DFS Recr."])
+        self.cbbMethods = QComboBox()
+        self.cbbMethods.addItems(["BFS", "DFS Iter.", "DFS Recr."])
 
         pane = QWidget()
 
         hbox = QHBoxLayout()
         hbox.addWidget(QLabel("Método de solução: "))
 
-        hbox.addWidget(cbbMethods)
+        hbox.addWidget(self.cbbMethods)
 
         pane.setLayout(hbox)
 
@@ -207,18 +228,18 @@ class Game:
 
     #--------------------------------------------------------------------------#
 
-    def step_solution(self):
+    def create_controls(self):
         """ Itera entre os passos que levam a solução (semelhante a depurar a solução) """
 
         pane = QWidget()
 
         hbox = QHBoxLayout()
         ControlButton.board = self.board
-        hbox.addWidget(ControlButton("<<", "back"))
-        hbox.addWidget(ControlButton("Solucionar", "solution"))
-        hbox.addWidget(ControlButton(">>", "foward"))
+        hbox.addWidget(ControlButton("<<",          "back",     self.board, self.pieces))
+        hbox.addWidget(ControlButton("Solucionar",  "solution", self.board, self.pieces))
+        hbox.addWidget(ControlButton(">>",          "foward",   self.board, self.pieces))
         pane.setLayout(hbox)
-
+    
         return pane
 
     #--------------------------------------------------------------------------#
@@ -256,4 +277,4 @@ class Game:
 #--------------------------------------------------------------------------#
 
 
-game = Game(3)
+game = Game(5)
